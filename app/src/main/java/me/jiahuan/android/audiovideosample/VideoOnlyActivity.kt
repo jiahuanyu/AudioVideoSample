78aba20e540d92cc4fa1823cf165317313fc9b2e
jiahuan.yu <abbottyu@xingren.com>
2020-01-09 10:02:58 +0800

    Initialize Commit

---
+++ app/src/main/java/me/jiahuan/android/audiovideosample/VideoOnlyActivity.kt
@@ -0,0 +1,15 @@
+package me.jiahuan.android.audiovideosample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_video_only.*
+
+class VideoOnlyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        start_record_button.setOnClickListener {
+
+        }
+    }
+}
