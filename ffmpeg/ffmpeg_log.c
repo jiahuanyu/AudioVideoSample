c1a90a111d925a916203e3e557292ec522697b4b
jiahuan.yu <abbottyu@xingren.com>
2020-01-17 19:06:09 +0800

    添加ffmpeg日志示例

---
+++ ffmpeg/ffmpeg_log.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <libavutil/log.h>
+
+int main(int argc, char *argv[])
+{
+    av_log_set_level(AV_LOG_DEBUG);
+    av_log(NULL, AV_LOG_DEBUG, "Hello %s!\n", "World");
+    return 0;
+}
